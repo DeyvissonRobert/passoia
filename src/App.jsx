aede3964b4934fb4d41e676a9060e3a9f7ad329d
DeyvissonRobert <robert.designerx@gmail.com>
2025-11-18 18:44:10 -0300

    iniciando

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+import Lancamentos from "./components/Lancamentos";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Lancamentos />
+    </>
+  );
+}
+
+export default App;
