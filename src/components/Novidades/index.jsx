cda38a3e4469284e6465f8a53f1d0f0a3685a9fe
DeyvissonRobert <robert.designerx@gmail.com>
2025-11-18 20:34:43 -0300

    Add H2 em novidades

--- src/components/Novidades/index.jsx
+++ src/components/Novidades/index.jsx
@@ -4,6 +4,7 @@ import make from "../../assets/make.png";
 function Novidades() {
   return (
     <section>
+      <h2>NOVIDADADES PARA VOCÊ</h2>
       <img src={make} alt="Banner da Revitalift" />
     </section>
   );
