0b6f9437eafacc0de10107ee1684dd6a7afdfc90
DeyvissonRobert <robert.designerx@gmail.com>
2025-11-19 16:34:22 -0300

    alterando nome do banner

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,10 +1,10 @@
 import "./hero.scss";
-import banner from "../../assets/banner.png";
+import bannerHero from "../../assets/banner.png";
 
 function Hero() {
   return (
     <section>
-      <img src={banner} alt="Banner da Revitalift" />
+      <img src={bannerHero} alt="Banner da Revitalift" />
     </section>
   );
 }
