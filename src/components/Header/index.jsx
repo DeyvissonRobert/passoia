00caa0957038e3856bccd77d32e85b3bb14123c9
DeyvissonRobert <robert.designerx@gmail.com>
2025-11-26 20:43:48 -0300

    Menu Hamburger no Header mobile

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,17 +1,53 @@
+import { useState, useEffect } from "react";
 import "./header.scss";
 import logo from "../../assets/logo.png";
 
 function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  // Fecha o menu automaticamente quando a tela ficar maior
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > 768) {
+        setIsOpen(false);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <header>
       <img src={logo} alt="logo" />
 
-      <ul>
-        <li className="links">LOOKS</li>
-        <li className="links">LANÇAMENTOS</li>
-        <li className="links">NOVIDADES</li>
+      {/* Botão hamburguer */}
+      <button
+        id="burger"
+        onClick={() => setIsOpen(!isOpen)}
+        className="material-symbols-outlined"
+      >
+        {isOpen ? "close" : "menu"}
+      </button>
+
+      {/* Menu */}
+      <ul className={`menu ${isOpen ? "open" : ""}`}>
+        <li className="links" onClick={() => setIsOpen(false)}>
+          <a href="#looks">LOOKS</a>
+        </li>
+
+        <li className="links" onClick={() => setIsOpen(false)}>
+          <a href="#lancamentos">LANÇAMENTOS</a>
+        </li>
+
+        <li className="links" onClick={() => setIsOpen(false)}>
+          <a href="#novidades">NOVIDADES</a>
+        </li>
       </ul>
+      
+      
     </header>
   );
 }
+
 export default Header;
